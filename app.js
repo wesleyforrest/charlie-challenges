9350822781bbc9e2e3aa54fa36c230dbcb0a796a
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-06-01 10:26:42 +0100

    challenges

---
+++ app.js
@@ -0,0 +1,20 @@
+const lowerCaseNames = ["charlie", "rob", "tea", "bert"];
+
+const toCapitalLetter = (arr) => {
+  const capitalArray = arr.map.charAt(0).toUpperCase;
+  return capitalArray;
+};
+
+console.log(lowerCaseNames);
+
+const arrayWithVowels = ["a", "b", "c", "d"];
+
+const removeVowels = (arr) => {
+  const withoutVowels = withoutVowels.map((withoutVowels) =>
+    withoutVowels.replace(/[aeiou]/g, "")
+  );
+  return withoutVowels;
+};
+removeVowels();
+
+console.log(removeVowels(arrayWithVowels));
